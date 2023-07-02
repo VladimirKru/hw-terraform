3e931a57c43e4aa0cfbb9c2715ef14f69594c6f1
vladimirkru <krytikov.vb@gmail.com>
2023-07-02 23:49:46 +0300

    ready_zadanie-3-2

--- terraform/3/src/disk_vm.tf
+++ terraform/3/src/disk_vm.tf
@@ -7,3 +7,44 @@ resource "yandex_compute_disk" "vol" {
     size = 1
       
 }
+
+resource "yandex_compute_instance" "vol-vm" {
+    name = "storage"
+    platform_id = "standard-v1"
+
+    resources {
+      cores = 2
+      memory = 1
+      core_fraction = 5
+    }
+
+    boot_disk {
+      initialize_params {
+        image_id = data.yandex_compute_image.ubuntu-2004-lts.image_id
+        type = "network-hdd"
+        size = 5
+      }
+    }
+
+    dynamic "secondary_disk" {
+        for_each = yandex_compute_disk.vol.*.id
+        content {
+          disk_id = secondary_disk.value
+        }
+    }
+
+    metadata = {
+        serial-port-enable = var.meta["ssh_root_key"].serial-port-enable
+        ssh-keys           = var.meta["ssh_root_key"].ssh_keys
+       
+    }
+
+    scheduling_policy {
+      preemptible = true
+    }
+
+    network_interface {
+      subnet_id = yandex_vpc_subnet.develop.id
+      nat       = true
+    }
+}
